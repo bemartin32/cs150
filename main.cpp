a52b1d4d3a404f031f75c8ab5a345b93076f94d5
Brady Martin <spitfire4141@Bradys-MBP.home>
2022-11-04 12:00:57 -0700

    One more cout statement

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"test3\n";
+    cout<<"Hello from local!\n";
+    cout<<"Hello from GitHub\n";
+    cout<<"I am back\n";
+    return 0;
+}
